4ab053f5eca7e810f958ec63921eba35c34e3605
Shota Teranishi <cattleya@MacBook-12inch.local>
2020-08-18 23:40:51 +0900

    typeinfo included (but no changes)

---
+++ dokushu_c++/code2-13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <typeinfo>
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    auto var01 = 256;
+    auto var02 = 3.14;
+    auto var03 = "yuri_ebihara";
+    auto var04 = true;
+
+    cout << typeid(var01).name() << endl
+         << typeid(var02).name() << endl
+         << typeid(var03).name() << endl
+         << typeid(var04).name() << endl;
+
+    const char *model = "ebichan";
+    cout << typeid(model).name() << endl;
+}
